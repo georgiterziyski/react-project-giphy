239c50d366b6f247f483749d0dd9389912e09fdb
georgiterziyski <56469632+georgiterziyski@users.noreply.github.com>
2020-01-31 00:52:18 +0200

    Adding Footer and Header Layouts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import React from 'react';
-import './App.css';
+import Header from './components/header/Header'
+import Home from './pages/Home'
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+//import reducers from "./redux/reducers";
+import thunk from 'redux-thunk';
+
+//const store = createStore(reducers, applyMiddleware(thunk));
+
+const Layout = props => (
+  <>
+    <Header />
+    <div className="container mt-5">
+      {props.children}
+    </div>
+  </>
+)
+
+const routes =[
+  {
+    path: '/',
+    exact: true,
+    main: () => <Layout>
+      <Home />
+    </Layout>
+  },
+  {
+    path: '/',
+    exact: true,
+    main: () => <Layout>
+      <Home />
+    </Layout>
+  }
+]
+
+const getRoutes = () => {
+  return routes.map((route, index) => {
+    return <Route
+      exact={route.exact}
+      key={index}
+      path={route.path}>
+      {route.main}
+    </Route>
+  })
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-        
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Router>
+    <Switch>
+      {getRoutes()}
+    </Switch>
+  </Router>
 }
 
 export default App;
--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export default {
-    baseURL: 'https://api.themoviedb.org/3/',
-    apiKey: 'ca3d69ee336e43d8099727f0d7ce3859',
-    basePosterURL: 'https://image.tmdb.org/t/p/w370_and_h556_bestv2/',
+    baseURL: 'http://api.giphy.com/v1/gifs/search?q=',
+    apiKey: 'DBFMyEd3Qs9z4ht48oGNqyk7E0GCMMzs',
+    limit: '&limit=32',
 };
